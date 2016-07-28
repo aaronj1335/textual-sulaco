46ce954a6ac1c6282d2b359cead8b90ad8089b64
Aaron Stacy <aaronstacy@google.com>
2016-07-28 14:23:20 -0500

    ui tweaks

--- scripts.js
+++ scripts.js
@@ -97,6 +97,7 @@ Sulaco = {
 
     setDescriptiveClassNames: function (lineNum) {
       var line = Sulaco.getLineEl(lineNum);
+      var nickRe = new RegExp(app.localUserNickname(), 'i');
 
       if (line.getAttribute('type') === 'debug' &&
           /Disconnect/.test(line.textContent))
@@ -108,8 +109,7 @@ Sulaco = {
       if (line.getAttribute('type') === 'topic')
         line.classList.add('topic');
 
-      if (line.getAttribute('type') === 'join' &&
-          /aaronj1335/i.test(line.textContent))
+      if (line.getAttribute('type') === 'join' && nickRe.test(line.textContent))
         line.classList.add('aaronj1335-join');
 
       if (line.getAttribute('type') === 'quit' &&
@@ -120,6 +120,25 @@ Sulaco = {
           /query/i.test(line.textContent))
         line.classList.add('join-query');
 
+    },
+
+    fixTimeStampFormatting: function(lineNum) {
+      var line = Sulaco.getLineEl(lineNum);
+      var time = line.querySelector('.time');
+
+      if (!time)
+        return;
+
+      var matches = time.textContent.match(/\[([^\]]+)\]/);
+      if (matches && matches[1]) {
+        var timeParts = matches[1].split(':');
+        var date = new Date();
+        date.setHours(timeParts[0]);
+        date.setMinutes(timeParts[1]);
+        var minutesString = date.getMinutes() < 10?
+          '0' + date.getMinutes() : date.getMinutes();
+        time.innerHTML = date.getHours() + ':' + minutesString;
+      }
     }
 };
 
@@ -131,6 +150,7 @@ Textual.newMessagePostedToView = function (lineNum) {
     Sulaco.handleBufferPlayback(lineNum);
     Sulaco.coalesceMessages(lineNum);
     Sulaco.setDescriptiveClassNames(lineNum);
+    Sulaco.fixTimeStampFormatting(lineNum);
 };
 
 Textual.viewFinishedLoading = function () {
